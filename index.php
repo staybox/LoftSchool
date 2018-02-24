d76dd21555b8804c54c63083882c71ec81599eb2
Andrey <staybox@gmail.com>
2018-02-24 22:06:42 +0300

    Сортировка массива по порядку

--- index.php
+++ index.php
@@ -6,9 +6,20 @@
  * Time: 18:59
  */
 
-echo "Hello World";
+/*  echo "Hello World";
 
-echo "Hello Second World2";
+    echo "Hello Second World2";
 
-echo "Hello Second World_3_4";
+    echo "Hello Second World_3_4_5";
+*/
 
+$data = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 1, 4181, 6765, 10946, 17711];
+
+sort($data);
+
+for($i=0; $i < count($data); $i++)
+{
+    "<pre>".
+    print_r($data[$i])
+    ."<pre/>";
+}
